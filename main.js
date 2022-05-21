975232ce79d97a8edd895957bdd4786abd12f9c1
Jessica Aresu <87132037+jessica-aresu@users.noreply.github.com>
2022-05-21 12:58:41 +0200

    Simple Form Validator

---
+++ main.js
@@ -0,0 +1,77 @@
+
+//VARIABLES DOM
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+//SHOWERROR FUNCTION
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+//SHOWSUCCESS FUNCTION
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className= 'form-control success'
+}
+//CHECK VALID EMAIL
+function checkEmail(email){
+    const re = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if (re.test(email.value.trim())){
+        showSuccess(email);
+    }else{
+        showError(email, `${getFieldName(email)} is not valid`);
+    }
+}
+function checkPassword(password){
+    const re = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
+    if(re.test(password.value.trim())){
+        showSuccess(password);
+    }else{
+        showError(password, `${getFieldName(password)} must contains lowercase and one numeric character or has at least one uppercase and one numeric character.`)
+        showError(password, `${getFieldName(password)} must contains lowercase and one numeric character or has at least one uppercase and one lowercase.`)
+    }
+}
+
+//CHECK PASSWORDS MATCH
+function checkPasswordsMatch(input1, input2){
+    if (input1.value !== input2.value){
+        showError(input2, `Passwords do not matchLe , please check them`)
+    }
+}
+//CHECK REQUIRED FIELDS
+function checkRequired(inputArr){
+    inputArr.forEach( input => {
+        if (input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    })
+}
+//CHECK LENGTH USERNAME E PASSWORD
+function checkLength(input, min, max){
+    if (input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`)
+    } else if (input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`)
+    } else{
+        showSuccess(input);
+    }
+}
+// GET FIELD NAME WITH CAPITAL LETTER
+function getFieldName(input){
+    return `${input.id.charAt(0).toUpperCase()}${input.id.slice(1)}`
+}
+//EVENT LISTENERS
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkEmail(email);
+    checkPassword(password);
+    checkPasswordsMatch(password, password2);
+})
